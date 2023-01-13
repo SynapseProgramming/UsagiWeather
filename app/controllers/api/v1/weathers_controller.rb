e282746fe00eecfe73c9907e2627d9d5796512a6
Roald <roald.ong@gmail.com>
2023-01-13 14:47:14 +0800

    Added in read api and update database on write

--- app/controllers/api/v1/weathers_controller.rb
+++ app/controllers/api/v1/weathers_controller.rb
@@ -2,11 +2,14 @@ class Api::V1::WeathersController < ApplicationController
 
   protect_from_forgery with: :null_session
   def create
-    render json: params_check
+    # render json: params_check
+    Weather.create!(params_check)
 
   end
 
   def read
+    @all_data = Weather.all
+    render json: @all_data
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post 'weathers/create'
+      get 'weathers/read'
     end
   end
 
