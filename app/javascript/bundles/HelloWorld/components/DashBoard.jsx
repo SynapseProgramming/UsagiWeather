52d13b210ff8b8146ec72e3727921bf99f3ce5a4
Roald <roald.ong@gmail.com>
2023-01-14 14:48:29 +0800

    Added in counter example

--- app/javascript/bundles/HelloWorld/components/DashBoard.jsx
+++ app/javascript/bundles/HelloWorld/components/DashBoard.jsx
@@ -3,7 +3,21 @@ import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const DashBoard = (props) => {
-  return <p>Dashboard</p>;
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Counter: {counter}</h1>
+    </div>
+  );
+
 };
 
 export default DashBoard;
