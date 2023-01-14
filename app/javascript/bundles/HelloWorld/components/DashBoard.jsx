2d835996f5e41c4b2046e7606273d7a06548c89a
Roald <roald.ong@gmail.com>
2023-01-14 15:24:44 +0800

    Added in update data function

--- app/javascript/bundles/HelloWorld/components/DashBoard.jsx
+++ app/javascript/bundles/HelloWorld/components/DashBoard.jsx
@@ -2,40 +2,37 @@ import PropTypes from "prop-types";
 import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-// const DeleteAll = () => {
-//   const url = "api/v1/weathers/destroy";
-//   let is_mounted = true;
-//   if (is_mounted) {
-//     fetch(url)
-//       .then((response) => {
-//         if (response.ok) {
-//           return response.json();
-//         }
-//         throw new Error("Network response was not ok.");
-//       })
-//       .then((response) => {
-//         Console.log("Everything was deleted!");
-//       })
-//       .catch((e) => console.error("Exception thrown", e.stack));
-//   }
-// };
-
 const DashBoard = (props) => {
-  const [counter, setCounter] = useState(0);
+  const [sensorData, setSensorData] = useState();
+
+  const updateData = () => {
+    const url = "api/v1/weathers/latest";
+    let is_mounted = true;
+    if (is_mounted) {
+      fetch(url)
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+          throw new Error("Network response was not ok.");
+        })
+        .then((response) => {
+          console.log(response);
+          //   Console.log("Everything was deleted!");
+        })
+        .catch((e) => console.error("Exception thrown", e.stack));
+    }
+  };
+
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      updateData();
     }, 1000);
 
     return () => clearInterval(interval);
   }, []);
 
-  return (
-    <div className="App">
-      <h1>Counter: {counter}</h1>
-    </div>
-  );
-
+  return <p>Dashboard!</p>;
 };
 
 export default DashBoard;
